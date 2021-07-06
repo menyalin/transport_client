f73cb6333e7a4f27efe666ddda78939d74997e66
Menyalin Alexey <menyalin@gmail.com>
2021-07-06 11:31:47 +0300

    #

--- src/socket.js
+++ src/socket.js
@@ -1,12 +1,15 @@
 import { io } from 'socket.io-client'
 
-const URL = 'http://localhost:3000'
+const URL = process.env.VUE_APP_API_WS_URL || 'http://localhost:3000'
 const socket = io(URL, { autoConnect: false })
 
 socket.onAny((event, args) => {
   console.log(event, args)
 })
 
+socket.on('error', (err) => {
+  console.log(err)
+})
 socket.on('connect_error', (err) => {
   if (err.message === 'invalid user') {
     console.log('Ошибка: хреновый UserID')
