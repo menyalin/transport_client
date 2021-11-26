eef4b1a6068fa062b728e4036b51f141fab031e8
Menyalin Alexey <menyalin@gmail.com>
2021-11-26 09:21:32 +0300

    ...

--- src/modules/profile/components/driverForm/medBook.vue
+++ src/modules/profile/components/driverForm/medBook.vue
@@ -4,39 +4,60 @@
       <app-block-title>{{ title }}</app-block-title>
     </div>
     <div class="med-book-wrapper">
-      <v-text-field
-        hide-details
-        outlined
-        :value="params.number"
-        dense
-        label="Номер"
-        @change="change($event, 'number')"
-      />
-      <app-date-time-input
-        label="Дата выдачи"
-        :value="params.issueDate"
-        hide-prepend-icon
-        hide-time-input
-        @change="change($event, 'issueDate')"
-      />
-      <app-date-time-input
-        label="Аттестация до"
-        :value="params.certifiedBeforeDate"
-        hide-prepend-icon
-        hide-time-input
-        @change="change($event, 'certifiedBeforeDate')"
-      />
-      <app-date-time-input
-        label="Ежегодная комиссия от"
-        :value="params.annualCommisionDate"
-        hide-prepend-icon
-        hide-time-input
-        @change="change($event, 'annualCommisionDate')"
-      />
+      <div class="first-column">
+        <v-text-field
+          hide-details
+          outlined
+          :value="params.number"
+          dense
+          label="Номер"
+          @change="change($event, 'number')"
+        />
+        <app-date-time-input
+          label="Дата выдачи"
+          :value="params.issueDate"
+          hide-prepend-icon
+          hide-time-input
+          @change="change($event, 'issueDate')"
+        />
+        <div class="date-input-row">
+          <app-date-time-input
+            label="Аттестация до"
+            :value="params.certifiedBeforeDate"
+            hide-prepend-icon
+            hide-time-input
+            @change="change($event, 'certifiedBeforeDate')"
+          />
+          <v-chip
+            v-if="certificateValidityPeriod"
+            class="my-1 mx-4"
+            :color="chipColor(certificateValidityPeriod)"
+          >
+            {{ certificateValidityPeriod }}
+          </v-chip>
+        </div>
+        <div class="date-input-row">
+          <app-date-time-input
+            label="Ежегодная комиссия от"
+            :value="params.annualCommisionDate"
+            hide-prepend-icon
+            hide-time-input
+            @change="change($event, 'annualCommisionDate')"
+          />
+          <v-chip
+            v-if="daysBeforeMedExamination"
+            class="my-1 mx-4"
+            :color="chipColor(daysBeforeMedExamination)"
+          >
+            {{ daysBeforeMedExamination }}
+          </v-chip>
+        </div>
+      </div>
       <v-textarea
         label="Примечание"
         :value="params.note"
         outlined
+        hide-details
         @change="change($event, 'note')"
       />
     </div>
@@ -46,6 +67,7 @@
 import { mapGetters } from 'vuex'
 import AppBlockTitle from './blockTitle.vue'
 import AppDateTimeInput from '@/modules/common/components/dateTimeInput'
+import moment from 'moment'
 
 export default {
   name: 'MedBook',
@@ -77,6 +99,19 @@ export default {
     fields() {
       return Object.keys(this.params)
     },
+    certificateValidityPeriod() {
+      if (!this.params.certifiedBeforeDate) return null
+      const todaySec = moment().unix()
+      const certDateSec = moment(this.params.certifiedBeforeDate).unix()
+      return Math.floor((certDateSec - todaySec) / (60 * 60 * 24))
+    },
+    daysBeforeMedExamination() {
+      const lastDate = moment(this.params.annualCommisionDate)
+        .add(1, 'year')
+        .unix()
+      const todaySec = moment().unix()
+      return Math.floor((lastDate - todaySec) / (60 * 60 * 24))
+    },
   },
   watch: {
     item: {
@@ -95,14 +130,29 @@ export default {
       this.params[field] = val
       this.$emit('change', this.params)
     },
+    chipColor(days) {
+      if (days < 14) return 'error'
+      if (days < 30) return 'warning'
+      return 'light-green'
+    },
   },
 }
 </script>
 <style scoped>
 .med-book-wrapper {
   display: grid;
-  grid-template-columns: 500px 300px 300px 300px;
+  grid-template-columns: 300px auto;
   margin: 10px;
   grid-gap: 15px;
+}
+.first-column {
+  display: grid;
+  grid-gap: 10px;
+}
+.date-input-row {
+  display: grid;
+  grid-template-columns: 3fr auto;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
