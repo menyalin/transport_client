46d798df9eeb85621b821ea843576f4c2937dceb
Menyalin Alexey <menyalin@gmail.com>
2022-03-10 12:37:57 +0300

    fix

--- src/modules/order/pages/orders/_putTableToClipboard.js
+++ src/modules/order/pages/orders/_putTableToClipboard.js
@@ -129,7 +129,7 @@ export default ({ items }) => {
         ? loadDepartureDate.toLocaleTimeString()
         : '-',
       unloadArrivalDate: unloadArrivalDate
-        ? loadArrivalDate.toLocaleDateString()
+        ? unloadArrivalDate.toLocaleDateString()
         : '-',
       unloadArrivalTime: unloadArrivalDate
         ? unloadArrivalDate.toLocaleTimeString()
