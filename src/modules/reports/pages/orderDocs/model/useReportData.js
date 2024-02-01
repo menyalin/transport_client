a3c7608f49065fee5f3f9ab76e39ea46c4a54aa2
Menyalin Alexey <menyalin@icloud.com>
2024-02-01 09:03:04 +0300

    fix orderDocs report

--- src/modules/reports/pages/orderDocs/model/useReportData.js
+++ src/modules/reports/pages/orderDocs/model/useReportData.js
@@ -1,7 +1,19 @@
 import { ref, watch } from 'vue'
 import { ReportService } from '@/shared/services'
+import { usePersistedRef } from '@/shared/hooks'
+import dayjs from 'dayjs'
 
-export default function ({ settings }) {
+export default function () {
+  const initialState = {
+    groupBy: 'month',
+    clients: [],
+    tks: [],
+    state: [],
+    getDocsDays: [],
+    reviewDocsDays: [],
+    date: new dayjs().format('YYYY-MM-DD'),
+  }
+  const settings = usePersistedRef(initialState, 'OrderDocsReport:settings')
   const items = ref([])
   const statisticData = ref({})
   const loading = ref(false)
@@ -43,6 +55,7 @@ export default function ({ settings }) {
   )
 
   return {
+    settings,
     items,
     statisticData,
     refresh,
