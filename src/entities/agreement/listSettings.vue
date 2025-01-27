5678060659e2b2492ab343dc1009894f1cc31ff4
Menyalin Alexey <menyalin@icloud.com>
2025-01-27 16:30:57 +0300

    add agreement settings

---
+++ src/entities/agreement/listSettings.vue
@@ -0,0 +1,42 @@
+import { AddressForm } from '@/entities/address';
+<template>
+  <div class="wrapper">
+    <v-text-field
+      :value="settings.search"
+      label="Поиск"
+      dense
+      hide-details
+      clearable
+      outlined
+      @change="updateSettings($event, 'search')"
+    />
+  </div>
+</template>
+<script>
+export default {
+  name: 'AgreementListSettings',
+  model: {
+    prop: 'settings',
+    event: 'change',
+  },
+  props: {
+    settings: Object,
+  },
+  setup(props, ctx) {
+    function updateSettings(value, field) {
+      ctx.emit('change', { ...props.settings.value, [field]: value })
+    }
+    return {
+      updateSettings,
+    }
+  },
+}
+</script>
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: row;
+  gap: 15px;
+  padding: 10px;
+}
+</style>
