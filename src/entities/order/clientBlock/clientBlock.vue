094193de08051cc7cc72162901c4b035b98f661c
Menyalin Alexey <menyalin@icloud.com>
2025-05-13 14:42:48 +0300

    fix client agreement selector

--- src/entities/order/clientBlock/clientBlock.vue
+++ src/entities/order/clientBlock/clientBlock.vue
@@ -6,31 +6,35 @@
     <div class="client-block">
       <v-autocomplete
         :items="clientItems"
-        v-model="params.client"
+        v-model="state.client"
         label="Заказчик"
         outlined
         :loading="loading"
         :messages="agreementNameSring"
-        :error="!agreement"
+        :error="!loading && !currentAgreement"
         dense
+        @change="changeClientHandler"
         :style="{ maxWidth: '400px' }"
+        :disabled="agreementDisabled"
       />
 
       <v-text-field
-        v-model.trim="params.num"
+        v-model.trim="state.num"
         outlined
         dense
         label="Номер заказа клиента"
         :errorMessages="numErrorMessages"
         :style="{ maxWidth: '250px' }"
+        @change="changeFieldHandler($event, 'num')"
       />
       <v-text-field
-        v-model.trim="params.auctionNum"
+        v-model.trim="state.auctionNum"
         outlined
         dense
         label="Номер аукциона"
         :errorMessages="auctionNumErrorMessages"
         :style="{ maxWidth: '250px' }"
+        @change="changeFieldHandler($event, 'auctionNum')"
       />
     </div>
     <v-btn
@@ -47,9 +51,7 @@
   </div>
 </template>
 <script>
-import { AgreementService } from '@/shared/services'
 import { BlockTitle } from '@/entities/order'
-
 import { useClientBlock } from './useClientBlock'
 
 export default {
@@ -62,51 +64,41 @@ export default {
     event: 'change',
   },
   props: {
-    item: Object,
     title: String,
+    item: Object,
     routeDate: String,
+    agreementDisabled: Boolean,
+    orderConfirmed: Boolean,
     carrier: String,
-    isValidNum: {
-      type: Boolean,
-      default: true,
-    },
-    isValidAuctionNum: {
-      type: Boolean,
-      default: true,
-    },
+    isValidNum: { type: Boolean, default: true },
+    isValidAuctionNum: { type: Boolean, default: true },
   },
-  setup() {
-    const { clientItems } = useClientBlock()
-    return { clientItems }
-  },
-  data() {
+  setup(props, ctx) {
+    const {
+      clientItems,
+      agreementNameSring,
+      currentAgreement,
+      state,
+      loading,
+      changeClientHandler,
+      changeFieldHandler,
+      changeAgreementHandler,
+      showChangeAgreementBtn,
+    } = useClientBlock(props, ctx)
     return {
-      tmpCarrier: null,
-      agreements: [],
-      agreement: null,
-      errorMessage: null,
-      loading: false,
-      params: {
-        client: null,
-        num: null,
-        auctionNum: null,
-        agreement: null,
-        directiveAgreement: false,
-      },
+      clientItems,
+      agreementNameSring,
+      currentAgreement,
+      state,
+      loading,
+      changeClientHandler,
+      changeFieldHandler,
+      showChangeAgreementBtn,
+      changeAgreementHandler,
     }
   },
+
   computed: {
-    agreementNameSring() {
-      const suffix = this.params.directiveAgreement
-        ? ' (Установлено вручную)'
-        : ''
-      return this.agreement
-        ? [this.agreement.name + suffix]
-        : ['Соглашение отсутствует']
-    },
-    showChangeAgreementBtn() {
-      return this.agreements.length > 1
-    },
     numErrorMessages() {
       return this.isValidNum
         ? []
@@ -116,109 +108,6 @@ export default {
       return this.isValidAuctionNum
         ? []
         : ['Номер аукциона не может быть пустым']
-    },
-  },
-  watch: {
-    carrier(val) {
-      if (val && val !== this.tmpCarrier) {
-        this.clearAgreement()
-        this.setAgreement()
-      }
-    },
-    item: {
-      immediate: true,
-      deep: true,
-      handler: function (val) {
-        this.params = { ...val }
-        this.getAgreements()
-      },
-    },
-
-    params: {
-      deep: true,
-      handler: function (val) {
-        const keys = Object.keys(this.params)
-        if (keys.some((key) => val[key] !== this.item[key])) {
-          this.$emit('change', this.params)
-        }
-      },
-    },
-    ['params.client']: {
-      handler: async function (newVal, oldVal) {
-        if (newVal !== oldVal) {
-          this.clearAgreement(true)
-          // await this.getAgreements()
-        }
-      },
-    },
-    routeDate: async function () {
-      this.$emit('change', {
-        ...this.params,
-        agreement: this.params.directiveAgreement
-          ? this.params.agreement
-          : null,
-      })
-    },
-  },
-  methods: {
-    changeAgreementHandler() {
-      if (!Array.isArray(this.agreements) || this.agreements.length < 2) return
-      const currentIdx = this.agreements.findIndex(
-        (i) => i?._id === this.agreement?._id
-      )
-      let nextIdx = 0
-      if (currentIdx !== -1 && currentIdx + 1 !== this.agreements.length)
-        nextIdx = currentIdx + 1
-      this.agreement = this.agreements[nextIdx]
-      this.params.agreement = this.agreement?._id
-      this.params.directiveAgreement = true
-    },
-
-    findAgreement(agreements, carrier) {
-      if (agreements.length === 0) return null
-      if (!carrier) return agreements[0]
-      return (
-        agreements.find((i) => i.allowedCarriers.includes(carrier)) ||
-        agreements[0]
-      )
-    },
-
-    setAgreement() {
-      if (this.params.agreement)
-        this.agreement = this.agreements.find(
-          (i) => i?._id === this.params.agreement
-        )
-      else {
-        this.agreement = this.findAgreement(this.agreements, this.carrier)
-        this.params.agreement = this.agreement?._id || null
-      }
-      this.$emit('updateAgreement', this.agreement)
-    },
-
-    async getAgreements() {
-      if (!this.routeDate || !this.params.client) return null
-      try {
-        this.loading = true
-        this.agreements = await AgreementService.getForClient({
-          client: this.params.client,
-          date: new Date(this.routeDate).toISOString(),
-        })
-        this.setAgreement()
-        this.tmpCarrier = this.carrier
-        this.loading = false
-      } catch (e) {
-        this.loading = false
-        this.errorMessage = e.message
-      }
-    },
-
-    clearAgreement(forceUpdate = false) {
-      if (forceUpdate || !this.params.directiveAgreement) {
-        this.params.agreement = null
-        this.agreement = null
-        this.directiveAgreement = false
-        this.$emit('updateAgreement', null)
-      }
     },
   },
 }
--- src/entities/order/clientBlock/useClientBlock.js
+++ src/entities/order/clientBlock/useClientBlock.js
@@ -1,9 +1,139 @@
+import { AgreementService } from '@/shared/services/index'
 import store from '@/store/index'
+import { ref, computed, watch, onMounted } from 'vue'
+import { clientAgreementSelector } from './utils/clientAgreementSelector'
 
-export const useClientBlock = () => {
+export const useClientBlock = (props, ctx) => {
+  const loading = ref(true)
+  const currentAgreement = ref(null)
+  const allowedAgreements = ref([])
+
+  const initialState = {
+    client: null,
+    num: null,
+    auctionNum: null,
+    agreement: null,
+    directiveAgreement: false,
+  }
+  const state = ref(initialState)
+
+  const allowToChangeAgreement = computed(() => {
+    if (props.orderConfirmed || state.value.directiveAgreement) return false
+    return true
+  })
+
+  function setAgreement() {
+    if (allowToChangeAgreement.value) {
+      currentAgreement.value = null
+      currentAgreement.value = clientAgreementSelector({
+        allowedAgreements: allowedAgreements.value,
+        carrierId: props.carrier,
+      })
+      state.value = {
+        ...state.value,
+        agreement: currentAgreement.value._id ?? null,
+        directiveAgreement: false,
+      }
+    } else {
+      currentAgreement.value = allowedAgreements.value.find(
+        (i) => i._id === state.value.agreement
+      )
+    }
+
+    ctx.emit('updateAgreement', currentAgreement.value)
+  }
+
+  async function getAllowedAgreements() {
+    if (!props.routeDate || !state.value.client) return
+    try {
+      loading.value = true
+      allowedAgreements.value = await AgreementService.getForClient({
+        client: state.value.client,
+        date: new Date(props.routeDate).toISOString(),
+        currentAgreementId: state.value.agreement,
+      })
+      setAgreement()
+    } catch (e) {
+      store.commit('setError', e)
+      console.error(e)
+    } finally {
+      loading.value = false
+    }
+  }
+  async function changeClientHandler(val) {
+    if (!val) return
+    await getAllowedAgreements()
+    ctx.emit('change', state.value)
+  }
+
+  function changeAgreementHandler() {
+    const idx =
+      allowedAgreements.value.findIndex(
+        (i) => i._id === state.value.agreement
+      ) || 0
+    currentAgreement.value =
+      allowedAgreements.value[(idx + 1) % allowedAgreements.value.length]
+
+    state.value = {
+      ...state.value,
+      agreement: currentAgreement.value._id,
+      directiveAgreement: true,
+    }
+    ctx.emit('change', state.value)
+  }
+  function changeFieldHandler(value, field) {
+    state.value = { ...state.value, [field]: value }
+    ctx.emit('change', state.value)
+  }
+
+  watch(
+    () => props.item,
+    async (val) => {
+      state.value = { ...val }
+    },
+    { deep: true, immediate: true }
+  )
+
+  watch(
+    () => props.carrier,
+    (newVal, oldVal) => {
+      if (newVal === oldVal) return
+      setAgreement()
+    }
+  )
+
+  onMounted(async () => {
+    // await getAllowedAgreements()
+  })
+  watch(
+    () => props.routeDate,
+    async () => await getAllowedAgreements(),
+    { immediate: true }
+  )
+
   return {
-    clientItems: store.getters.partners
-      .filter((p) => p.isClient)
-      .map((i) => ({ value: i._id, text: i.name })),
+    state,
+    loading,
+    currentAgreement,
+    changeClientHandler,
+    changeFieldHandler,
+    changeAgreementHandler,
+    clientItems: computed(() =>
+      store.getters.partners
+        .filter((p) => p.isClient)
+        .map((i) => ({ value: i._id, text: i.name }))
+    ),
+    showChangeAgreementBtn: computed(
+      () => allowedAgreements.value?.length > 1 && !props.agreementDisabled
+    ),
+
+    agreementNameSring: computed(() => {
+      const suffix = state.value.directiveAgreement
+        ? ' (Установлено вручную)'
+        : ''
+      return currentAgreement.value
+        ? [currentAgreement.value.name + suffix]
+        : ['Соглашение отсутствует']
+    }),
   }
 }
