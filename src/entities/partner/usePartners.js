3436ebd2de33cb02377ec63ebb3ff2a260cfbdae
Menyalin Alexey <menyalin@icloud.com>
2025-12-22 13:19:16 +0300

    add client filter

--- src/entities/partner/usePartners.js
+++ src/entities/partner/usePartners.js
@@ -4,6 +4,7 @@ import store from '@/store'
 export const usePartners = () => {
   return {
     allPartners: store.getters.partners,
+    clients: store.getters.partners.filter((i) => i.isClient),
     actions: {
       create: ({ query }) => router.push({ name: 'PartnerCreate', query }),
       update: ({ id, query }) =>
