3c870ee2f51d21f3f103487cb527fc398561a1a0
Menyalin Alexey <menyalin@icloud.com>
2026-02-18 15:27:50 +0300

    refactor(staffRolesSelect): замена v-select на список v-checkbox
    - Изменён UI выбора ролей с выпадающего списка на список чекбоксов
    - Добавлена функция toggleRole для переключения роли при клике
    - Добавлен caption "Роли пользователя" вместо label внутри компонента

--- src/features/worker/staffRolesSelect.vue
+++ src/features/worker/staffRolesSelect.vue
@@ -1,13 +1,17 @@
 <template>
-  <v-select
-    :value="roles"
-    :items="allRoles"
-    multiple
-    label="Роли пользователя"
-    outlined
-    :disabled="disabled"
-    @change="changeHandler"
-  />
+  <div>
+    <div class="text-caption text--secondary mb-2">Роли пользователя</div>
+    <v-checkbox
+      v-for="role in allRoles"
+      :key="role.value"
+      :label="role.text"
+      :input-value="roles?.includes(role.value)"
+      :disabled="disabled"
+      hide-details
+      class="mt-0 pt-0"
+      @change="toggleRole(role.value)"
+    />
+  </div>
 </template>
 <script>
 import { computed, getCurrentInstance } from 'vue'
@@ -26,17 +30,25 @@ export default {
       default: true,
     },
   },
-  setup(_, ctx) {
+  setup(props, ctx) {
     const { proxy } = getCurrentInstance()
 
-    function changeHandler(val) {
-      ctx.emit('change', val)
+    function toggleRole(roleValue) {
+      const currentUserRoles = props.roles || []
+      let newVal = []
+      if (currentUserRoles.includes(roleValue))
+        newVal = currentUserRoles.filter((r) => r !== roleValue)
+      else newVal = [...currentUserRoles, roleValue]
+
+      ctx.emit('change', newVal)
     }
+
     const allRoles = computed(() => {
       return proxy.$store.getters.staffRoles
     })
+
     return {
-      changeHandler,
+      toggleRole,
       allRoles,
     }
   },
